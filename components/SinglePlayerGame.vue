953177793dca4a3a7b5305f55f7a9330b4c69629
Carlos Vaz <carlosfelipe_2002@outlook.com>
2022-07-10 00:50:56 -0300

    first version of flaggame

---
+++ components/SinglePlayerGame.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="main-container">
+    <div class="top-labels">
+    <span>Home</span>
+    <span>Challenge: 1/10</span>
+    </div>
+    <div class="challenge-flag-wrapper">
+      <img id="challenge-flag" :src="challengeFlagPath" alt="">
+    </div>
+    <div class="challenge-options">
+      <button class="button-default-style" @click="challengeFlag = 'kh'">Khmer</button>
+      <button class="button-default-style" @click="challengeFlag = 'nc'">Nepali</button>
+      <button class="button-default-style" @click="challengeFlag = 'kr'">Korean</button>
+      <button class="button-default-style" @click="challengeFlag = 'lr'">Lurkish</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      challengeFlagPath: 'https://static.significados.com.br/flags/br.svg',
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .challenge-flag-wrapper{
+    padding-top: 3rem;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  #challenge-flag {
+    width: 20rem;
+    height: auto;
+  }
+
+  .challenge-options{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .top-labels{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .top-labels span{
+    font-size: 1.5rem;
+    color: #E5E5E5;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <section class="title-section">
+      <h1>Guess The Flag</h1>
+      <img :src="homeFlagPath" id="home-flag">
+    </section>
+    <section class="home-action-buttons main-container" v-if="gameMode == ''">
+      <p>Challenge yourself or your friends!</p>
+      <button class="button-default-style" @click="gameMode = 'singleplayer'">Singleplayer Challenge</button>
+      <button class="button-default-style">Multiplayer Challenge</button>
+    </section>
+    <section v-if="gameMode == 'singleplayer'">
+      <SinglePlayerGame />
+    </section>
+    <footer>
+      <p>Made by Carlos Vaz&copy; 2022</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import SinglePlayerGame from '~/components/SinglePlayerGame.vue';
+export default {
+  components: {
+    SinglePlayerGame
+  },
+  data(){
+    return {
+      homeFlagPath: 'https://static.significados.com.br/flags/br.svg',
+      flagPaths: [
+        'https://static.significados.com.br/flags/kh.svg',
+        'https://static.significados.com.br/flags/nc.svg',
+        'https://static.significados.com.br/flags/kr.svg',
+        'https://static.significados.com.br/flags/lr.svg',
+        'https://static.significados.com.br/flags/ca.svg',
+      ],
+      gameMode: '',
+    }
+  },
+  created(){
+    setInterval(() => {
+        this.homeFlagPath = this.flagPaths[Math.floor(Math.random() * this.flagPaths.length)];
+    }, 3000);
+  }
+}
+</script>
+
+<style scoped>
+.title-section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+}
+
+#home-flag {
+  width: 6rem;
+  height: auto;
+  margin-left: 1.5rem;
+}
+
+.title-section h1 {
+  font-size: 3rem;
+  color: #fff;
+}
+
+.home-action-buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 25rem;
+}
+
+.home-action-buttons p {
+  color: #E5E5E5;
+  font-size: 1.3rem;
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+  height: 2rem;
+  background-color: #0f192e;
+  width: 100%;
+  color: rgb(77, 77, 77);
+  display: flex;
+  align-items: center;
+  padding-left: 1rem;
+}
+</style>
